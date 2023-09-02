45063f1b6d58caf03bbd705cdb19dd920e218f3f
Görkem <a.gorkemunuvar@gmail.com>
2023-09-02 20:06:01 +0200

    chore: init flutter project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FundaApp());
+}
+
+class FundaApp extends StatelessWidget {
+  const FundaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
